c332c249046afc8bbed8d4f05f2841e4ee38595d
Alex <beichuanalex@gmail.com>
2023-05-26 21:28:56 +0800

    初步调通小程序>后端>数据库的一整条链路

--- pages/add/add.js
+++ pages/add/add.js
@@ -1,1 +1,39 @@
-Page({})
+var query = `
+query AcctTypes($type:Int!){
+  acctTypes(type:$type) {
+    id
+    sort
+    type
+    icon
+    iconName
+  }
+}
+`
+
+Page({
+  data: {
+    type: 2,
+    acctTypes: []
+  },
+  onLoad: function () {
+    wx.request({
+      url: 'http://127.0.0.1:3000/graphql',
+      method: 'POST',
+      header: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      data: JSON.stringify({
+        query,
+        variables: {
+          type: this.data.type
+        }
+      }),
+      success: (res) => {
+        this.setData({
+          acctTypes: res.data.data.acctTypes.sort((a, b) => a.sort - b.sort)
+        })
+      }
+    })
+  }
+})

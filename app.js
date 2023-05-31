81326299630e8ba7c9cc987cde4e023aeecbd95a
Alex <beichuanalex@gmail.com>
2023-06-01 04:38:08 +0800

    完成底部弹窗布局

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 // app.js
 App({
-  
+  globalData: {
+    bottomSafeHeight: 0
+  },
+  onLaunch: function () {
+    try {
+      const res = wx.getSystemInfoSync()
+      this.globalData.bottomSafeHeight = res.screenHeight - res.safeArea.bottom
+    } catch (e) {
+      console.log(e)
+    }
+  }
 })
--- pages/add/add.js
+++ pages/add/add.js
@@ -2,17 +2,24 @@ import {
   getAcctTypes
 } from './request'
 
+var app = getApp()
+
 Page({
   data: {
     type: 2,
     selectedId: -1,
-    acctTypes: []
+    acctTypes: [],
+    bottomSafeHeight: 0
   },
   onLoad: function () {
-    getAcctTypes.call(this, this.data.type)
+    getAcctTypes.call(this, this.data.type);
+    this.setData({
+      bottomSafeHeight : app.globalData.bottomSafeHeight
+    });
   },
   handleTab: function (event) {
     this.setData({
+      selectedId: -1,
       type: event.currentTarget.dataset.type
     });
     getAcctTypes.call(this, this.data.type)

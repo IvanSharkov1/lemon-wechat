899e3877241b8e6cca8f4e1995cfc44a97b47379
Alex <beichuanalex@gmail.com>
2023-06-03 03:04:12 +0800

    Normalize all the line endings

--- pages/add/add.js
+++ pages/add/add.js
@@ -9,12 +9,14 @@ Page({
     type: 2,
     selectedId: -1,
     acctTypes: [],
-    bottomSafeHeight: 0
+    bottomSafeHeight: 0,
+    money: 0,
+    remark: "",
   },
   onLoad: function () {
     getAcctTypes.call(this, this.data.type);
     this.setData({
-      bottomSafeHeight : app.globalData.bottomSafeHeight
+      bottomSafeHeight: app.globalData.bottomSafeHeight
     });
   },
   handleTab: function (event) {
@@ -28,5 +30,28 @@ Page({
     this.setData({
       selectedId: event.currentTarget.dataset.id
     })
+  },
+  handleKeyboard: function (event) {
+    var keyValue = event.currentTarget.dataset.value
+    if (typeof keyValue === 'number' || keyValue === '+' || keyValue === '-' || keyValue === '.') {
+      this.setData({
+        money: keyValue
+      })
+    }
+    if (keyValue === 'btn-delete') {
+
+    }
+
+    if (keyValue === '再记') {
+
+    }
+    if (keyValue === '保存') {
+      const saveData = {
+        acctTypeId: this.data.selectedId,
+        amount: this.data.money,
+        remark: this.data.remark,
+      }
+      console.log(saveData)
+    }
   }
 })
